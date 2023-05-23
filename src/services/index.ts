fb7a0dc20dc95f4d68f6c6dbc89c91781ac7e600
Manuel2u <edodoo011@st.ug.edu.gh>
2023-05-23 07:53:30 +0000

    initialized user service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,16 @@
-
-
+import { IAppContext } from "../types/app";
+import UserService from "./user";
 
 export interface IServices {
+  UserService: UserService;
+}
 
+export function initServices(context: IAppContext) {
+  try {
+    return {
+      user: new UserService(context),
+    };
+  } catch (e) {
+    throw e;
+  }
 }
