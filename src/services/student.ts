b7b429b109c1e340470ff90966cab1dcb92e0638
shedeurbarker <abarker@st.ug.edu.gh>
2023-11-09 15:46:09 +0000

    fix bug
    Register existing student

--- src/services/student.ts
+++ src/services/student.ts
@@ -3,193 +3,191 @@ import { ICreateStudentInput } from "../types/students";
 import { __generateQuery } from "../utils/query";
 
 export default class StudentService extends IService {
-  constructor(context: IAppContext) {
-    super(context);
-  }
-
-  async createOne(input: ICreateStudentInput, { user }, session) {
-    try {
-      if (!user) throw new Error("Unauthorized");
-
-      const _student = await this.db.studentModel.findOne({
-        studentID: input.studentID,
-      });
-
-      if (_student) throw new Error("Student Already Exist");
-
-      const student = new this.db.studentModel({
-        ...input,
-        year: "2023-2024",
-        createdBy: user._id,
-      });
-      await student.save({ session });
-      return student;
-    } catch (e) {
-      console.log(e);
-      throw e;
+    constructor(context: IAppContext) {
+        super(context);
     }
-  }
-  async updateOne(input, { user }, session) {
-    try {
-      console.log("STUDENT_INPUT", input);
 
-      if (!user) throw new Error("Unauthorized");
+    async createOne(input: ICreateStudentInput, { user }, session) {
+        try {
+            if (!user) throw new Error("Unauthorized");
 
-      const _student = await this.db.studentModel.findOne({
-        _id: input.studentObjectID,
-      });
+            const _student = await this.db.studentModel.findOne({
+                studentID: input.studentID,
+                year: "2023-2024",
+            });
 
-      if (!_student) throw new Error("Student does not Exist");
+            if (_student) throw new Error("Student Already Exist");
 
-      await _student.updateOne(
-        {
-          $set: {
-            ...input,
-            updatedBy: user._id,
-          },
-        },
-        { session }
-      );
-      return "Student Updated Successfully";
-    } catch (e) {
-      throw e;
+            const student = new this.db.studentModel({
+                ...input,
+                year: "2023-2024",
+                createdBy: user._id,
+            });
+            await student.save({ session });
+            return student;
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
     }
-  }
-  async deleteOne({ input }, { user }, session) {
-    try {
-      if (!user) throw new Error("Unauthorized");
+    async updateOne(input, { user }, session) {
+        try {
+            console.log("STUDENT_INPUT", input);
 
-      const student = await this.db.studentModel.findByIdAndDelete(
-        input.studentID,
-        { session }
-      );
-      if (!student) throw new Error("student Not Found");
-      return user;
-    } catch (e) {
-      throw e;
+            if (!user) throw new Error("Unauthorized");
+
+            const _student = await this.db.studentModel.findOne({
+                _id: input.studentObjectID,
+            });
+
+            if (!_student) throw new Error("Student does not Exist");
+
+            await _student.updateOne(
+                {
+                    $set: {
+                        ...input,
+                        updatedBy: user._id,
+                    },
+                },
+                { session }
+            );
+            return "Student Updated Successfully";
+        } catch (e) {
+            throw e;
+        }
     }
-  }
-  async getMany({ filter, search, populate, pagination }, { user }) {
-    try {
-      if (!user) throw new Error("Unauthorized");
+    async deleteOne({ input }, { user }, session) {
+        try {
+            if (!user) throw new Error("Unauthorized");
 
-      const generatedQuery = __generateQuery("Student", {
-        filter,
-        search,
-        populate: populate,
-        pagination,
-        sort: { createdAt: "desc" },
-      });
-
-      const students = this.db.studentModel
-        .find(generatedQuery.filter)
-        .sort(generatedQuery.sort)
-        .skip(generatedQuery.skip)
-        .limit(generatedQuery.limit)
-        .populate(generatedQuery.populate)
-        .exec();
-
-      const studentCount = await this.db.studentModel.countDocuments(
-        generatedQuery.filter
-      );
-
-      if (!students) {
-        throw new Error("students Not Found");
-      }
-
-      return {
-        students,
-        studentCount,
-      };
-    } catch (e) {
-      throw e;
+            const student = await this.db.studentModel.findByIdAndDelete(input.studentID, {
+                session,
+            });
+            if (!student) throw new Error("student Not Found");
+            return user;
+        } catch (e) {
+            throw e;
+        }
     }
-  }
-  async getOne({ filter, search, populate }, { user }) {
-    try {
-      if (!user) throw new Error("Unauthorized");
+    async getMany({ filter, search, populate, pagination }, { user }) {
+        try {
+            if (!user) throw new Error("Unauthorized");
 
-      const generatedQuery = __generateQuery("Student", {
-        filter,
-        populate,
-        search,
-      });
+            const generatedQuery = __generateQuery("Student", {
+                filter,
+                search,
+                populate: populate,
+                pagination,
+                sort: { createdAt: "desc" },
+            });
 
-      const student = await this.db.studentModel
-        .find(generatedQuery.filter)
-        .sort(generatedQuery.sort)
-        .populate(generatedQuery.populate)
-        .exec();
+            const students = this.db.studentModel
+                .find(generatedQuery.filter)
+                .sort(generatedQuery.sort)
+                .skip(generatedQuery.skip)
+                .limit(generatedQuery.limit)
+                .populate(generatedQuery.populate)
+                .exec();
 
-      if (!student) {
-        throw new Error("student Not Found");
-      }
+            const studentCount = await this.db.studentModel.countDocuments(generatedQuery.filter);
 
-      return student;
-    } catch (e) {
-      throw e;
+            if (!students) {
+                throw new Error("students Not Found");
+            }
+
+            return {
+                students,
+                studentCount,
+            };
+        } catch (e) {
+            throw e;
+        }
     }
-  }
+    async getOne({ filter, search, populate }, { user }) {
+        try {
+            if (!user) throw new Error("Unauthorized");
 
-  async countLevel100({ filter }, { user }) {
-    console.log(filter);
+            const generatedQuery = __generateQuery("Student", {
+                filter,
+                populate,
+                search,
+            });
 
-    try {
-      if (!user) throw new Error("Unauthorized");
+            const student = await this.db.studentModel
+                .find(generatedQuery.filter)
+                .sort(generatedQuery.sort)
+                .populate(generatedQuery.populate)
+                .exec();
 
-      const count = await this.db.studentModel.countDocuments({
-        year: filter.year.eq,
-        level: "100",
-      });
+            if (!student) {
+                throw new Error("student Not Found");
+            }
 
-      return count;
-    } catch (e) {
-      throw e;
+            return student;
+        } catch (e) {
+            throw e;
+        }
     }
-  }
 
-  async countLevel200({ filter }, { user }) {
-    try {
-      if (!user) throw new Error("Unauthorized");
+    async countLevel100({ filter }, { user }) {
+        console.log(filter);
 
-      const count = await this.db.studentModel.countDocuments({
-        year: filter.year.eq,
-        level: "200",
-      });
+        try {
+            if (!user) throw new Error("Unauthorized");
 
-      return count;
-    } catch (e) {
-      throw e;
+            const count = await this.db.studentModel.countDocuments({
+                year: filter.year.eq,
+                level: "100",
+            });
+
+            return count;
+        } catch (e) {
+            throw e;
+        }
     }
-  }
 
-  async countLevel300({ filter }, { user }) {
-    try {
-      if (!user) throw new Error("Unauthorized");
+    async countLevel200({ filter }, { user }) {
+        try {
+            if (!user) throw new Error("Unauthorized");
 
-      const count = await this.db.studentModel.countDocuments({
-        year: filter.year.eq,
-        level: "300",
-      });
+            const count = await this.db.studentModel.countDocuments({
+                year: filter.year.eq,
+                level: "200",
+            });
 
-      return count;
-    } catch (e) {
-      throw e;
+            return count;
+        } catch (e) {
+            throw e;
+        }
     }
-  }
 
-  async countLevel400({ filter }, { user }) {
-    try {
-      if (!user) throw new Error("Unauthorized");
+    async countLevel300({ filter }, { user }) {
+        try {
+            if (!user) throw new Error("Unauthorized");
 
-      const count = await this.db.studentModel.countDocuments({
-        year: filter.year.eq,
-        level: "400",
-      });
+            const count = await this.db.studentModel.countDocuments({
+                year: filter.year.eq,
+                level: "300",
+            });
 
-      return count;
-    } catch (e) {
-      throw e;
+            return count;
+        } catch (e) {
+            throw e;
+        }
     }
-  }
+
+    async countLevel400({ filter }, { user }) {
+        try {
+            if (!user) throw new Error("Unauthorized");
+
+            const count = await this.db.studentModel.countDocuments({
+                year: filter.year.eq,
+                level: "400",
+            });
+
+            return count;
+        } catch (e) {
+            throw e;
+        }
+    }
 }
